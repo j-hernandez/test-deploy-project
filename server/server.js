727db53b68a9205b2ab966877e88bfb99db52f84
James Hernandez <james@j-hernandez.com>
2020-07-18 12:55:33 -0400

    Updates

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('New API Changes'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
